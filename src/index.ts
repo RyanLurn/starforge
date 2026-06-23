ec876ad80e6d8c1a755a3e4395312bba9e46904a
Ryan Luong <lumintriet@gmail.com>
2026-06-23 10:27:47 +0700

    feat: asking for the target directory.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import { intro, outro } from "@clack/prompts";
+#!/usr/bin/env bun
+
+import { isCancel, cancel, intro, outro, text } from "@clack/prompts";
+import { z } from "zod";
+
+import { CURRENT_DIRECTORY } from "@/constants";
 
 intro("Gravity is ready. Time to forge a star.");
+
+const targetDirectory = await text({
+  message: "Where will the star be born?",
+  placeholder: CURRENT_DIRECTORY,
+  defaultValue: CURRENT_DIRECTORY,
+  initialValue: CURRENT_DIRECTORY,
+  validate: z.string().default(CURRENT_DIRECTORY),
+});
+
+if (isCancel(targetDirectory)) {
+  cancel("Process canceled.");
+  process.exit(1);
+}
 
 outro("The void is expanding... See you again in millennia.");
